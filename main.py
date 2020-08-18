ffdd860d9be781b17d808551ca69cc5d96d05edd
Logan Lee Goodrum <68272865+ShadowLogan@users.noreply.github.com>
2020-08-18 02:22:28 +0100

    V1
    First version of the Star Wars GUI button window opener.

---
+++ main.py
@@ -0,0 +1,50 @@
+import tkinter as tk
+from tkinter import *
+import threading
+import winsound
+
+
+window = Tk()
+window.config(bg="black")
+starwars = tk.Label(
+    text="A long time ago in a galaxy far far away...",
+    fg="lightBlue",
+    bg="black",
+    height=800,
+    width=400,
+    font=(None, 20)
+)
+window.title("Star Wars")
+window.geometry("800x400")
+starwars.pack()
+
+
+def play():
+    winsound.PlaySound("swtheme.wav", winsound.SND_ALIAS)
+
+
+def printtext():
+    threadsound.start()
+    print("STAR WARS")
+    window2 = Tk()
+    window2.config(bg="black")
+    starwarsnew = tk.Label(
+        window2,
+        text="STAR WARS",
+        fg="yellow",
+        bg="black",
+        height=800,
+        width=400,
+        font=(None, 80)
+    )
+    window2.title("STAR WARS")
+    window2.geometry("800x400")
+    starwarsnew.pack()
+
+
+threadsound = threading.Thread(target=play)
+
+button = Button(window, text="PLAY", fg="lightBlue", bg="black", width=10, command=printtext).place(x=350, y=300)
+
+
+starwars.mainloop()
